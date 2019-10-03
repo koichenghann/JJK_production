8f3ad9a941fb4c72b5a4dda70dc0a050f49ddde4
ChengHannKoi <chengghannkoi@gmail.com>
2019-10-04 00:21:56 +0800

    example for object in array then array in object

---
+++ DIP210_Assignment/koi/tempDB.js
@@ -0,0 +1,7 @@
+var Residence = new Array(2);
+var Units = new Array(2);
+Residence[0] = {residenceID:1, address:"kl", numUnits:1, sizePerUnit:100, monthlyRental:100,
+                units:  Units[0] = {unitNo:100, availability:true}};
+
+
+console.log((Residence[0].units).unitNo)
