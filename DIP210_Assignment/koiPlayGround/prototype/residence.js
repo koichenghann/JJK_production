8421c3d39ab057416375072a8d4291cbcd8c9c89
ChengHannKoi <chengghannkoi@gmail.com>
2019-10-06 00:32:05 +0800

    update

--- DIP210_Assignment/koiPlayGround/prototype/residence.js
+++ DIP210_Assignment/koiPlayGround/prototype/residence.js
@@ -1,11 +1,11 @@
-var dummyResidence = [{residenceID:"A001", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"A002", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"A003", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"B210", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"B201", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"B208", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"MPU4", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
-                      {residenceID:"ELM2", address:"No.911, Jalan Jalan, Kampung madosa, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},];
+var dummyResidence = [{residenceID:"A001", address:"No.911, Jalan Jalan, Kampung gantut, 54321 sini", units:100, unitSize:3000, monthlyRental: 12000},
+                      {residenceID:"A002", address:"No.912, Jalan roads, Kampung madosa, 12312 sana", units:320, unitSize:2400, monthlyRental: 14214},
+                      {residenceID:"A003", address:"No.817, Jalan nanan, Kampung sentos, 23552 mana", units:761, unitSize:1500, monthlyRental: 23525},
+                      {residenceID:"B210", address:"No.889, Jalan kiri_, Kampung gotaas, 23523 mama", units:561, unitSize:1111, monthlyRental: 34634},
+                      {residenceID:"B201", address:"No.770, Jalan kanan, Kampung baboru, 23523 papa", units:791, unitSize:8787, monthlyRental: 12444},
+                      {residenceID:"B208", address:"No.001, Jalan left_, Kampung mamoru, 45774 poko", units:781, unitSize:8888, monthlyRental: 37347},
+                      {residenceID:"MPU4", address:"No.003, Jalan right, Kampung kansai, 23423 piko", units:087, unitSize:6666, monthlyRental: 28884},
+                      {residenceID:"ELM2", address:"No.023, Jalan mati_, Kampung kyoton, 74574 nono", units:418, unitSize:1088, monthlyRental: 23574},];
 var disMode;
 
 //incomplete
@@ -20,25 +20,25 @@ function hideCol(){
 }
 
 
-//incomplete
-function deleteRow(row){
-  //var table = document.getElementById("leftPane").getElementsByTagName("tbody")[0].deleteRow[row];
 
-}
-
-
-function populateTable(table){
-  //var table = document.getElementById("leftPane").getElementsByTagName("tbody")[0];
-  //table.innerHTML = "jeff";
-
-  for(var i=0; i<table.length; i++){
-    insertRow(i, dummyResidence);
+//generic
+function populateTable(dataSource){
+  for(var i=0; i<dataSource.length; i++){
+    insertRow(dataSource, i);
   }
 }
 
+//generic
+function clearTable(table){
+  var rowCount = document.getElementById(table).getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
+  for(var i=rowCount-1; i=>0; i--){
+    deleteRow(table, i);
+  }
+}
 
-function insertRow(item, dataSource){
-  var table = document.getElementById("leftPane").getElementsByTagName("tbody")[0];
+ //nonGeneric for residence table only
+function insertRow(dataSource, item){
+  var table = document.getElementById("residenceTable").getElementsByTagName("tbody")[0];
   var newRow = table.insertRow(table.length);
 
 
@@ -62,8 +62,18 @@ function insertRow(item, dataSource){
   No.setAttribute("class", disMode);
 }
 
+//generic
+function deleteRow(table, row){
+  document.getElementById(table).getElementsByTagName("tbody")[0].deleteRow(row);
+}
 
+
+
+//nonGeneric for showing residence form only
+//but inteligent
 function showForm(selectedRow){
+   //element.setAttribute('style', element.getAttribute('style')+'; color: red');
+
   document.getElementById("rightPane").innerHTML = `<form class="" action="index.html" method="post">
                                                       <div class="form-group">
                                                         <label for="residenceID_in">Residence ID</label>
@@ -75,7 +85,7 @@ function showForm(selectedRow){
                                                       </div>
                                                       <div class="form-group">
                                                         <label for="units_in">Units</label>
-                                                        <input type="text" class="form-control" name="" id="units" value="" placeholder="Enter Units">
+                                                        <input type="text" class="form-control" name="" id="units_in" value="" placeholder="Enter Units">
                                                       </div>
                                                       <div class="form-group">
                                                         <label for="unitSize_in">Unit Size</label>
@@ -87,5 +97,33 @@ function showForm(selectedRow){
                                                       </div>
                                                     </form>`;
 
-    document.getElementById("residenceID_in").value = selectedRow.cells[0].innerHTML;
+
+
+
+
+    //document.getElementById("residenceID_in").value = selectedRow.cells[0].innerHTML;
+    var rowNo = selectedRow.cells[0].innerHTML;
+    var tempElement = document.getElementById("rightPane").getElementsByTagName("form")[0].getElementsByTagName("input")[2];
+
+
+    var residenceID     = document.getElementById("residenceID_in");
+    var address         = document.getElementById("address_in");
+    var units           = document.getElementById("units_in");
+    var unitSize        = document.getElementById("unitSize_in");
+    var monthlyRental   = document.getElementById("monthlyRental_in");
+
+    residenceID.value   = selectedRow.cells[1].innerHTML;
+    address.value       = selectedRow.cells[2].innerHTML;
+    units.value         = selectedRow.cells[3].innerHTML;
+    unitSize.value      = selectedRow.cells[4].innerHTML;
+    monthlyRental.value = selectedRow.cells[5].innerHTML;
+
+    if(selectedRow != null){
+      residenceID       .setAttribute("readonly", null);
+      address           .setAttribute("readonly", null);
+      units             .setAttribute("readonly", null);
+      unitSize          .setAttribute("readonly", null);
+      monthlyRental     .setAttribute("readonly", null);
+    }
+
 }
