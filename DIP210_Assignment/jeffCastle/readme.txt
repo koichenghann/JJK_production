8366d10efedf29148f9f6bf160e8a3c111482477
martinjohn27 <martinjohn24517@gmail.com>
2019-10-05 16:23:48 +0800

    e

---
+++ DIP210_Assignment/jeffCastle/readme.txt
@@ -0,0 +1,18 @@
+
+
+             ___          ___           ___         ___
+            /  /\        /  /\         /  /\       /  /\
+           /  /:/       /  /:/_       /  /:/_     /  /:/_
+          /__/::\      /  /:/ /\     /  /:/ /\   /  /:/ /\
+          \__\/\:\    /  /:/ /:/_   /  /:/ /:/  /  /:/ /:/
+             \  \:\  /__/:/ /:/ /\ /__/:/ /:/  /__/:/ /:/
+              \__\:\ \  \:\/:/ /:/ \  \:\/:/   \  \:\/:/
+              /  /:/  \  \::/ /:/   \  \::/     \  \::/
+             /__/:/    \  \:\/:/     \  \:\      \  \:\
+             \__\/      \  \::/       \  \:\      \  \:\
+                         \__\/         \__\/       \__\/
+
+
+1. hi Jeff, i built a castle for you, no need to thanks me.
+2. how is business today, did those guy visit your store.
+3. this folder is for you to push assignment file, you can do your assignment here.
--- DIP210_Assignment/koiPlayGround/prototype/residence.js
+++ DIP210_Assignment/koiPlayGround/prototype/residence.js
@@ -10,13 +10,15 @@ var dummyResidence = [{residenceID:"A001", address:"No.911, Jalan Jalan, Kampung
 
 function populateTable(table){
   for(var i=0; i<table.length; i++){
-    insertTableRow(i);
+    insertTableRow(i, dummyResidence);
   }
 }
 
-function insertTableRow(item){
-  var table = document.getElementById("residenceTable").getElementsByTagName("tbody")[0];
+
+function insertTableRow(item, dataSource){
+  var table = document.getElementById("leftPane").getElementsByTagName("tbody")[0];
   var newRow = table.insertRow(table.length);
+
 
   No            = newRow.insertCell(0);
   residenceID   = newRow.insertCell(1);
@@ -24,19 +26,19 @@ function insertTableRow(item){
   unitSize      = newRow.insertCell(3);
   monthlyRental = newRow.insertCell(4);
   units         = newRow.insertCell(5);
+                  newRow.setAttribute("onClick", "showForm(this)");
+
 
   No.innerHTML            = 001
-  residenceID.innerHTML   = dummyResidence[item].residenceID;
-  address.innerHTML       = dummyResidence[item].address;
-  units.innerHTML         = dummyResidence[item].units;
-  unitSize.innerHTML      = dummyResidence[item].unitSize;
-  monthlyRental.innerHTML = dummyResidence[item].monthlyRental;
+  residenceID.innerHTML   = dataSource[item].residenceID;
+  address.innerHTML       = dataSource[item].address;
+  units.innerHTML         = dataSource[item].units;
+  unitSize.innerHTML      = dataSource[item].unitSize;
+  monthlyRental.innerHTML = dataSource[item].monthlyRental;
 }
 
 
-
-
-function ffinsertNewRecord(data) {
+function ffinsertNewRecord_for_reference_only(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
@@ -50,4 +52,36 @@ function ffinsertNewRecord(data) {
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
+}
+
+
+function showForm(residenceSelection){
+  if (residenceSelection == null){
+
+  }
+  document.getElementById("rightPane").innerHTML = `<form class="" action="index.html" method="post">
+                                                      <div class="form-group">
+                                                        <label for="residenceID_in">Residence ID</label>
+                                                        <input type="text" class="form-control" name="" id="residenceID_in" value="" placeholder="Enter Residence ID">
+                                                      </div>
+                                                      <div class="form-group">
+                                                        <label for="address_in">Address</label>
+                                                        <input type="text" class="form-control" name="" id="address_in" value="" placeholder="Enter Address">
+                                                      </div>
+                                                      <div class="form-group">
+                                                        <label for="units_in">Units</label>
+                                                        <input type="text" class="form-control" name="" id="units" value="" placeholder="Enter Units">
+                                                      </div>
+                                                      <div class="form-group">
+                                                        <label for="unitSize_in">Unit Size</label>
+                                                        <input type="text" class="form-control" name="" id="unitSize_in" value="" placeholder="Enter Unit Size  (square feet)">
+                                                      </div>
+                                                      <div class="form-group">
+                                                        <label for="monthlyRental_in">Monthly Rental</label>
+                                                        <input type="text" class="form-control" name="" id="monthlyRental_in" value="" placeholder="Enter Monthly Rental (MYR)">
+                                                      </div>
+                                                    </form>
+                                                    <b><p id="selectedUnit"></P></b>
+                                                    `;
+  document.getElementById("selectedUnit").innerHTML = this.units;
 }
