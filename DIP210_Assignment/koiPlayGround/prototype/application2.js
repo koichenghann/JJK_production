e77ba7079f7acdc47238914f773e6f5995271e95
ChengHannKoi <chengghannkoi@gmail.com>
2019-10-07 12:06:15 +0800

    update

--- DIP210_Assignment/koiPlayGround/prototype/application2.js
+++ DIP210_Assignment/koiPlayGround/prototype/application2.js
@@ -1,25 +1,30 @@
 
-var dummyApplication = [{applicationID:1, applicantID:3, residenceID:5, officerID:1},
-                        {applicationID:5, applicantID:4, residenceID:8, officerID:1},
-                        {applicationID:4, applicantID:1, residenceID:1, officerID:1},
-                        {applicationID:3, applicantID:1, residenceID:1, officerID:1},
-                        {applicationID:2, applicantID:1, residenceID:1, officerID:1},
-                        {applicationID:6, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:7, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:8, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:9, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:10, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:11, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:12, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:13, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:14, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:15, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:16, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:17, applicantID:1, residenceID:1, officerID:2},
-                        {applicationID:18, applicantID:1, residenceID:1, officerID:2}];
-var dummyApplicant = [{applicantID:3, applicantName}]
-var applicationList = dummyApplication;
-var residenceList = JSON.parse(localStorage.residence);
+var dummyApplication = [{applicationID:1, applicantID:3, residenceID:"A001", officerID:1},
+                        {applicationID:5, applicantID:4, residenceID:"A002", officerID:1},
+                        {applicationID:4, applicantID:1, residenceID:"A003", officerID:1},
+                        {applicationID:3, applicantID:1, residenceID:"B210", officerID:1},
+                        {applicationID:2, applicantID:1, residenceID:"MPU4", officerID:1},
+                        {applicationID:6, applicantID:1, residenceID:"B201", officerID:2},
+                        {applicationID:7, applicantID:1, residenceID:"B208", officerID:2},
+                        {applicationID:8, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:9, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:10, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:11, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:12, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:13, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:14, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:15, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:16, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:17, applicantID:1, residenceID:"1", officerID:2},
+                        {applicationID:18, applicantID:1, residenceID:"1", officerID:2}];
+
+var dummyApplicant   = [{applicantID:3, username:"jeff", td:0},
+                        {applicantID:4, username:"jacky",td:0},
+                        {applicantID:1, username:"koi", td:0}];
+
+var applicationList  = dummyApplication;
+var applicantList = dummyApplicant;
+var residenceList    = JSON.parse(localStorage.residence);
 
 
 
@@ -34,7 +39,7 @@ function populateTable(){
 
 
 
-function insertRow(inNo, inCell1, inCell2){
+function insertRow(inNo, inApplicantID, inResidenceID){
   var table = document.getElementById("applicationListTable").getElementsByTagName("tbody")[0];
   newRow = table.insertRow(table.length);
 
@@ -55,29 +60,56 @@ function insertRow(inNo, inCell1, inCell2){
   cell1.setAttribute("class", "hidden");
   cell4.setAttribute("class", "statusNew");
 
-  applicantName =
-  residenceID = inCell2
-  cell3.getElementsByTagName("div")[0].innerHTML = inCell1;
-  cell3.getElementsByTagName("div")[1].innerHTML = inCell2;
+  for(var i=0; i<applicantList.length; i++){
+    if(applicantList[i].applicantID == inApplicantID){
+      applicantName = applicantList[i].username;
+      break;
+    }
+  }
+
+  /*
+  for(var i=0; i<residenceList.length; i++){
+    if(residenceList[i].residenceID == inResidenceID){
+      residenceID = residenceList[i].residenceID;
+    }
+  }*/
+
+  cell3.getElementsByTagName("div")[0].innerHTML = applicantName;
+  cell3.getElementsByTagName("div")[1].innerHTML = inResidenceID;
 
 }
 
 
 
 function displayForm(row){
+  //document.getElementById("formPane").innerHTML =  row.cells[1].innerHTML;
   var applicationID = row.cells[1].innerHTML;
-  var applicationObject;
-  //alert(applicationID);
+
+  var application;
   for(var i=0; i<applicationList.length; i++){
     if(applicationList[i].applicationID == applicationID){
-      //alert("found: " + i);
+      application = applicationList[i];
       break;
     }
   }
-  var residenceObject;
-  for(var i=0; i>residenceList.length; i++){
 
+  var residence;
+  for(var i=0; i<residenceList.length; i++){
+    if(residenceList[i].residenceID == application.residenceID){
+      residence = residenceList[i];
+      break;
+    }
   }
 
-  document.getElementById("rightPane")
+  var applicant;
+  for(var i=0; i<applicantList.length; i++){
+    if(applicantList[i].applicantID == application.applicantID){
+      applicant = applicantList[i];
+      break;
+    }
+  }
+
+  document.getElementById("formPane").innerHTML   = `application:` + application.applicationID
+                                                + `<br>applicant:` + applicant.username
+                                                + `<br>residence:` + residence.residenceID;
 }
