e769cea75e9ea6b871c7abe50ea9715ac816f70b
martinjohn27 <martinjohn24517@gmail.com>
2019-10-10 23:55:04 +0800

    View Residence and Submit Application layout finalised (Left JavaScipt)

--- DIP210_Assignment/jackyTower/residence.js
+++ DIP210_Assignment/jackyTower/residence.js
@@ -1,3 +1,14 @@
+//Jaya Apartment
+//Dummy data of residence object
+var residence1 = {residenceName:"Bunga Jaya Apartment", residenceID:"A00B176", address:"58, Jalan Kepong, Pekan Kepong, 52000 Kuala Lumpur,", unitSize:"505 Sqft", amenities:"Walking distance to KTM", availableUnit:"215", monthlyRental:"RM 680"};
+var residence2 = {residenceName:"Tolong Jaya Condominium", residenceID:"K00B339", address:"No. 15, Jalan Sri Semantan 1, Off, Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur,", unitSize:"735 Sqft", amenities:"Walking distance to MRT", availableUnit:"99", monthlyRental:"RM 850"};
+
+//Array to store a collection of residence object
+var residenceList =[residence1,residence2];
+
+//set each details of residence into the hthml
+document.getElementById("residenceName").innerHTML = "Bunga Jaya Apartment";
+
 
 //paste search value into the input box
 var searchResidence = localStorage.searchVal;
@@ -8,6 +19,21 @@ document.getElementById("inputArea").value = searchArea;
 
 var searchCity = localStorage.searchCity;
 document.getElementById("inputCity").value = searchCity;
+
+function searchResidence() {
+    var searchVal = document.getElementById("inputSearch").value;
+    localStorage.searchVal = searchVal;
+
+    var searchArea = document.getElementById("inputArea").value;
+    localStorage.searchArea = searchArea;
+
+    var searchCity = document.getElementById("inputCity").value;
+    localStorage.searchCity = searchCity;
+}
+
+
+
+
 
 
 //receive selected residence details
@@ -58,11 +84,18 @@ function selectedResidence1(){
 }
 
 
+function requiredDateAndYear(){
+  var sm = document.getElementById("inputMonth");
+  var month = sm.options[sm.selectedIndex].value;
+  localStorage.requiredMonth = month;
 
+  var sy = document.getElementById("inputYear");
+  var year = sy.options[sy.selectedIndex].value;
+  localStorage.requiredYear = year;
+}
 
-
-
-
+console.log(localStorage.requiredMonth);
+console.log(localStorage.requiredYear);
 //alert(document.getElementById("inputArea").getElementsByTagName("option")[1].value);
 /*
 for(var i=0; i<document.getElementById("inputArea").getElementsByTagName("option").length; i++){
--- DIP210_Assignment/jackyTower/submission.js
+++ DIP210_Assignment/jackyTower/submission.js
@@ -1,5 +1,22 @@
 
 //call value of each Attribute in application form into the input textbox
+
+localStorage.fullName = "Jason";
+localStorage.email = "jason666@gmail.com";
+localStorage.monthlyIncome = "RM3000";
+
+//user's profile details
+var fullName = localStorage.fullName;
+document.getElementById("inputFullName").value = fullName;
+
+var email = localStorage.email;
+document.getElementById("inputEmail").value = email;
+
+var monthlyIncome = localStorage.monthlyIncome;
+document.getElementById("inputIncome").value = monthlyIncome;
+
+
+//selected residence details
 var residenceName = localStorage.residenceName;
 document.getElementById("inputResidenceName").value = residenceName;
 
@@ -18,10 +35,22 @@ document.getElementById("inputUnitSize").value = unitSize;
 var monthlyRental = localStorage.monthlyRental;
 document.getElementById("inputMonthlyRental").value = monthlyRental;
 
+//Required Month and Year info
+var requiredYear = localStorage.requiredYear;
+document.getElementById("inputYear").value = requiredYear;
 
+var requiredMonth = localStorage.requiredMonth;
+document.getElementById("inputMonth").value = requiredMonth;
+
+//Cancel Button onclick action
+function cancel() {
+  window.history.back();
+}
 console.log(residenceName);
 console.log(residenceID);
 console.log(address);
 console.log(amenities);
 console.log(unitSize);
 console.log(monthlyRental);
+console.log(requiredYear);
+console.log(requiredMonth);
