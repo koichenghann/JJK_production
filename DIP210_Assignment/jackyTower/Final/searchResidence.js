ac272e670a4fce09e2dacfdcb508f1a42faca99e
martinjohn27 <martinjohn24517@gmail.com>
2019-10-14 01:11:35 +0800

    Recover data lost

--- DIP210_Assignment/jackyTower/Final/searchResidence.js
+++ DIP210_Assignment/jackyTower/Final/searchResidence.js
@@ -17,6 +17,25 @@ function yy(){
 }
 */
 
+var residenceList = JSON.parse(localStorage.residence);
+
+document.getElementById("a1").innerHTML = residenceList[0].residenceName;
+document.getElementById("a2").innerHTML = residenceList[0].residenceID;
+document.getElementById("a3").innerHTML = "RM" + residenceList[0].monthlyRental;
+document.getElementById("a4").addEventListener("click", function(){localStorage.searchArea=document.getElementById("inputArea").value;localStorage.searchCity=document.getElementById("inputCity").value; localStorage.searchVal = residenceList[0].residenceName; window.location.href = "residence_page.html";});
+
+
+document.getElementById("b1").innerHTML = residenceList[1].residenceName;
+document.getElementById("b2").innerHTML = residenceList[1].residenceID;
+document.getElementById("b3").innerHTML = "RM" + residenceList[1].monthlyRental;
+document.getElementById("b4").addEventListener("click", function(){localStorage.searchArea=document.getElementById("inputArea").value;localStorage.searchCity=document.getElementById("inputCity").value; localStorage.searchVal = residenceList[1].residenceName; window.location.href = "residence_page.html";});
+
+document.getElementById("c1").innerHTML = residenceList[2].residenceName;
+document.getElementById("c2").innerHTML = residenceList[2].residenceID;
+document.getElementById("c3").innerHTML = "RM" + residenceList[2].monthlyRental;
+document.getElementById("c4").addEventListener("click", function(){localStorage.searchArea=document.getElementById("inputArea").value;localStorage.searchCity=document.getElementById("inputCity").value; localStorage.searchVal = residenceList[2].residenceName; window.location.href = "residence_page.html";});
+
+
 //bring search value to viewResidence page
 function searchResidence() {
     var searchVal = document.getElementById("inputSearch").value;
