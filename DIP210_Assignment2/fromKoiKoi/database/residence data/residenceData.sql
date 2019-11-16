85a50dce3b86c4c4055dfeb6cb347951aa08e0e9
martinjohn27 <martinjohn24517@gmail.com>
2019-11-17 01:26:11 +0800

    Create sql file - residenceData
    will be updated again next morning

---
+++ DIP210_Assignment2/fromKoiKoi/database/residence data/residenceData.sql
@@ -0,0 +1,4 @@
+USE mhsmysql;
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES ('R001','Tolong Jaya Condominium','No. 15, Jalan Sri Semantan 1, Off, Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur','120','735','850','');
