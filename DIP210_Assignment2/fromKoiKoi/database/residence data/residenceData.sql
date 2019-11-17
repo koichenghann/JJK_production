494a3acd7586ff2f4296c27d61d6d59863ab852a
martinjohn27 <martinjohn24517@gmail.com>
2019-11-17 21:48:28 +0800

    Full Version of Residence Data
    Updates

--- DIP210_Assignment2/fromKoiKoi/database/residence data/residenceData.sql
+++ DIP210_Assignment2/fromKoiKoi/database/residence data/residenceData.sql
@@ -1,4 +1,38 @@
 USE mhsmysql;
 
 INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
-VALUES ('R001','Tolong Jaya Condominium','No. 15, Jalan Sri Semantan 1, Off, Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur','120','735','850','');
+VALUES ('R001','Tolong Jaya Condominium','Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur','120','735','850','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R002','Desa Damansara Condominium','Jalan Setiakasih, Bukit Damansara, 50490 Kuala Lumpur','85','780','880','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R003','Park Seven Condominium','Jalan Tujuh, Kuala Lumpur City Centre, 50450 Kuala Lumpur','85','780','880','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R004','One KL','Jalan Satu, Kuala Lumpur City Centre, 50450 Kuala Lumpur','50','930','950','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R005','Le Chato Apartments','Jalan Raya, Bukit Seputeh, 50460 Kuala Lumpur','100','620','600','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R006','Taman Bantu Jaya','Jalan Duta, Bukit Damansara, 50490 Kuala Lumpur','100','620','600','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R007','Apartment Prima','Jalan Prima 2, Kepong, 52100 Kuala Lumpur','110','700','750','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R008','Nova 2 Apartment','Jalan Nova 2, Bukit Segambut, 51200 Kuala Lumpur','110','700','750','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R009','Setiawangsa Residence','Jalan Setia, Bukit Dinding, 54200 Kuala Lumpur','200','880','900','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R010','Residensi Razakmas','Jalan Bakti, Bandar Tun Razak, 56000 Kuala Lumpur','200','880','900','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R011','Taman Sungai Sangat Bersih','Jalan Sangat Bersih, Sungai Besi, 57000 Kuala Lumpur','70','630','599','');
+
+INSERT INTO residence (residenceID, residenceName, address, unitCount, unitSize, montlyRental, staffID)
+VALUES('R012','Kepong Jaya Condominium','Jalan Kepong Baru, Kepong, 52100 Kuala Lumpur','70','630','599','');
+
