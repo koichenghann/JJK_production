6b55cc77805050f02fc43e67dd6922c2720f9ad3
martinjohn27 <martinjohn24517@gmail.com>
2019-11-18 19:54:56 +0800

    Updated viewResidence

--- DIP210_Assignment2/fromKoiKoi/residence_page.js
+++ DIP210_Assignment2/fromKoiKoi/residence_page.js
@@ -1,30 +1,61 @@
-validateUser("applicant");
+var xmlhttp = new XMLHttpRequest();
+var response =  function(){};
+var currentUser;
+var residenceList;
 
-var dummyList2 = [    {residenceID:"A001", residenceName:"Twins Residence", address:"2, Jalan Damanlela, Pusat Bandar Damansara, 50490 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur", unitCount:100, unitSize:3000, monthlyRental: 12000.00, amenities: undefined, units: new Array, staffID: "KOI001"},
-                      {residenceID:"A002", residenceName:"jeff Penthouse", address:"No.912, Jalan roads, Kampung madosa, 12312 sana", unitCount:320, unitSize:2400, monthlyRental: 14214.00, amenities: "new Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"A003", residenceName:"Koi Resort", address:"No.817, Jalan nanan, Kampung sentos, 23552 mana", unitCount:761, unitSize:1500, monthlyRental: 23525.00, amenities: "new Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"A004", residenceName:"no name", address:"No.889, Jalan kiri_, Kampung gotaas, 23523 mama", unitCount:561, unitSize:1111, monthlyRental: 34634.00, amenities: "new Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"B208", residenceName:"undefined", address:"No.770, Jalan kanan, Kampung baboru, 23523 papa", unitCount:791, unitSize:8787, monthlyRental: 12444.00, amenities: "new Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"B210", residenceName:"null", address:"No.001, Jalan left_, Kampung mamoru, 45774 poko", unitCount:781, unitSize:8888, monthlyRental: 37347.00, amenities: "new Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"MPU4", residenceName:"not error", address:"No.003, Jalan right, Kampung kansai, 23423 piko", unitCount:087, unitSize:6666, monthlyRental: 28884.00, amenities: "new Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"B201", residenceName:"empty", address:"No.023, Jalan mati_, Kampung kyoton, 74574 nono", unitCount:418, unitSize:1088, monthlyRental: 23574.00, amenities: "ew Array", units: new Array, staffID: "KOI001"},
-                      {residenceID:"K00B339", residenceName:"Tolong Jaya Condominium", address:"No. 15, Jalan Sri Semantan 1, Off, Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur,", unitCount:99, unitSize:735, monthlyRental: 850.00, amenities: "Walking distance to MRT", units: new Array, staffID: "KOI001"},
-                      {residenceID:"A00B176", residenceName:"Bunga Jaya Apartment", address:"58, Jalan Kepong, Pekan Kepong, 52000 Kuala Lumpur,", unitCount:215, unitSize:505, monthlyRental: 680.00, amenities: "Walking distance to KTM", units: new Array, staffID: "KOI001"},];
-
-//localStorage.residence = "";
-if(localStorage.residence==""||localStorage.residence==undefined){
-  localStorage.residence = JSON.stringify(dummyList2);
-  //console.log("start 1 : "+localStorage.residence);
+//function to validate user
+function validateUser(){
+  response = function(responseText){
+    if (responseText=="1") {
+      getCurrentUser(); //call function to get currentUser
+    }
+    else {
+      window.location.href="login.html";
+    }
+  };
+  submit("key=validate&userType=applicant");
 }
 
-//Jaya Apartment
-//Dummy data of residence object
-var dummyResidence = [{residenceName:"Bunga Jaya Apartment", residenceID:"A00B176", address:"58, Jalan Kepong, Pekan Kepong, 52000 Kuala Lumpur,", unitSize:"505 Sqft", amenities:"Walking distance to KTM", UnitCount:"215", monthlyRental:"RM 680"},
-                      {residenceName:"Tolong Jaya Condominium", residenceID:"K00B339", address:"No. 15, Jalan Sri Semantan 1, Off, Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur,", unitSize:"735 Sqft", amenities:"Walking distance to MRT", UnitCount:"99", monthlyRental:"RM 850"}];
+//function to get currentUser
+function getCurrentUser(){
+  response = function(responseText){
+    if (responseText!="") {
+      currentUser = JSON.parse(responseText);
+      console.log(currentUser);
+      getAllResidences();
+      //document.getElementById("form_username").innerHTML = currentUser.username;
+      //document.getElementById("form_staffID").innerHTML = currentUser.staffID;
+      //getStatistic();
+    }
+  };
+  submit("key=getCurrentUser");
+}
 
-//Array to store a collection of residence object
-//ar residenceList =[residence1,residence2];
-var residenceList = JSON.parse(localStorage.residence);
+validateUser();
+
+
+function getAllResidences(){
+
+  response = function(responseText){
+    residenceList  = JSON.parse(responseText);
+    //console.log('jeff gg: ' + responseText);
+    searchResidence();
+  };
+  submit('key=getAllResidences');
+}
+
+
+function submit(message){
+  xmlhttp.open("POST", "residence_page.php");
+  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xmlhttp.send(message);
+  xmlhttp.onload = function(){
+                      console.log(this.responseText);
+                      response(this.responseText);
+                    };
+}
+
+
 
 
 //paste search value into the input box
@@ -167,12 +198,6 @@ function requiredDateAndYear(){
 
 
 
-
-
-
-
-
-
 //          ___           ___           ___           ___           ___           ___
 //         /  /\         /  /\         /  /\         /  /\         /  /\         /__/\
 //        /  /:/_       /  /:/_       /  /::\       /  /::\       /  /:/         \  \:\
@@ -269,146 +294,3 @@ function searchEngine(source,criteria1,criteria2,criteria3){
 //       \__\/ /:/     \  \:\/:/     \  \:\        \  \:\        \  \:\/:/     \  \:\
 //         /__/:/       \  \::/       \  \:\        \  \:\        \  \::/       \  \:\
 //         \__\/         \__\/         \__\/         \__\/         \__\/         \__\/
-
-
-
-
-
-
-
-
-
-
-
-
-//old js code
-
-//alert(document.getElementById("inputArea").getElementsByTagName("option")[1].value);
-/*
-for(var i=0; i<document.getElementById("inputArea").getElementsByTagName("option").length; i++){
-  (document.getElementById("inputArea").getElementsByTagName("option")
-  if(document.getElementById("inputArea").getElementsByTagName("option")[i].value == searchArea){
-    document.getElementById("inputArea").getElementsByTagName("option")[i].setAttribute("selected", "");
-  }
-}
-console.log(searchArea);
-*/
-
-
-
-
-
-
-
-
-
-
-/*
-//Jaya Apartment
-//Dummy data of residence object
-var residence1 = {residenceName:"Bunga Jaya Apartment", residenceID:"A00B176", address:"58, Jalan Kepong, Pekan Kepong, 52000 Kuala Lumpur,", unitSize:"505 Sqft", amenities:"Walking distance to KTM", availableUnit:"215", monthlyRental:"RM 680"};
-var residence2 = {residenceName:"Tolong Jaya Condominium", residenceID:"K00B339", address:"No. 15, Jalan Sri Semantan 1, Off, Jalan Semantan, Bukit Damansara, 50490 Kuala Lumpur,", unitSize:"735 Sqft", amenities:"Walking distance to MRT", availableUnit:"99", monthlyRental:"RM 850"};
-
-//Array to store a collection of residence object
-var residenceList =[residence1,residence2];
-
-//set each details of residence into the hthml
-document.getElementById("residenceName").innerHTML = "Bunga Jaya Apartment";
-
-
-//paste search value into the input box
-var searchResidence = localStorage.searchVal;
-document.getElementById("inputSearch").value= searchResidence;
-
-var searchArea = localStorage.searchArea;
-document.getElementById("inputArea").value = searchArea;
-
-var searchCity = localStorage.searchCity;
-document.getElementById("inputCity").value = searchCity;
-
-function searchResidence() {
-    var searchVal = document.getElementById("inputSearch").value;
-    localStorage.searchVal = searchVal;
-
-    var searchArea = document.getElementById("inputArea").value;
-    localStorage.searchArea = searchArea;
-
-    var searchCity = document.getElementById("inputCity").value;
-    localStorage.searchCity = searchCity;
-}
-
-
-
-
-
-
-//receive selected residence details
-function selectedResidence(){
-  var residenceName = document.getElementById("residenceName").innerHTML;
-  localStorage.residenceName = residenceName;
-
-  var residenceID = document.getElementById("residenceID").innerHTML;
-  localStorage.residenceID = residenceID;
-
-  var address = document.getElementById("address").innerHTML;
-  localStorage.address = address;
-
-  var unitSize = document.getElementById("unitSize").innerHTML;
-  localStorage.unitSize = unitSize;
-
-  var amenities = document.getElementById("amenities").innerHTML;
-  localStorage.amenities = amenities;
-
-  var availableUnit = document.getElementById("availableUnit").innerHTML;
-  localStorage.availableUnit = availableUnit;
-
-  var monthlyRental = document.getElementById("monthlyRental").innerHTML;
-  localStorage.monthlyRental = monthlyRental;
-}
-
-function selectedResidence1(){
-  var residenceName1 = document.getElementById("residenceName1").innerHTML;
-  localStorage.residenceName = residenceName1;
-
-  var residenceID1 = document.getElementById("residenceID1").innerHTML;
-  localStorage.residenceID = residenceID1;
-
-  var address1 = document.getElementById("address1").innerHTML;
-  localStorage.address = address1;
-
-  var unitSize1 = document.getElementById("unitSize1").innerHTML;
-  localStorage.unitSize = unitSize1;
-
-  var amenities1 = document.getElementById("amenities1").innerHTML;
-  localStorage.amenities = amenities1;
-
-  var availableUnit1 = document.getElementById("availableUnit1").innerHTML;
-  localStorage.availableUnit = availableUnit1;
-
-  var monthlyRental1 = document.getElementById("monthlyRental1").innerHTML;
-  localStorage.monthlyRental = monthlyRental1;
-}
-
-
-function requiredDateAndYear(){
-  var sm = document.getElementById("inputMonth");
-  var month = sm.options[sm.selectedIndex].value;
-  localStorage.requiredMonth = month;
-
-  var sy = document.getElementById("inputYear");
-  var year = sy.options[sy.selectedIndex].value;
-  localStorage.requiredYear = year;
-}
-
-console.log(localStorage.requiredMonth);
-console.log(localStorage.requiredYear);
-//alert(document.getElementById("inputArea").getElementsByTagName("option")[1].value);
-/*
-for(var i=0; i<document.getElementById("inputArea").getElementsByTagName("option").length; i++){
-  (document.getElementById("inputArea").getElementsByTagName("option")
-  if(document.getElementById("inputArea").getElementsByTagName("option")[i].value == searchArea){
-    document.getElementById("inputArea").getElementsByTagName("option")[i].setAttribute("selected", "");
-  }
-}
-console.log(searchArea);
-*/
---
+++ DIP210_Assignment2/fromKoiKoi/residence_page.php
@@ -0,0 +1,171 @@
+<?php
+  //start session
+  session_start();
+
+  $currentUser = "";
+  $currentUserType = "";
+
+  //validate currentUser
+  if(isset($_SESSION['currentUser'])){
+    $currentUser = $_SESSION['currentUser'];
+  }
+  else{
+    echo "";
+  }
+
+  //validate currentUserType
+  if(isset($_SESSION['currentUserType'])){
+    $currentUserType = $_SESSION['currentUserType'];
+  }
+  else{
+    echo "";
+  }
+
+  //connect to a database
+  $dbServername = 'localhost'; /*default servername of XAMPP*/
+  $dbUsername = 'root'; /*default username of XAMPP*/
+  $dbPassword = ''; /*default password of XAMPP*/
+  $dbName = 'mhsmysql'; /*this is the database name*/
+
+  //establish connection to Databse
+  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+  //key declaration, to get pointing value
+  $key = $_POST['key'];
+
+
+  //switch case statement to specific internal function to be triggered
+  switch($key){
+
+    //to validate user in view residence use case page
+    case 'validate':
+      echo validateUser($currentUser, $currentUserType);
+      break;
+
+    //to get the type of user (currentType), either "applicant" or "housingOfficer"
+    case 'getCurrentUser':
+      echo json_encode($currentUser);
+      break;
+
+    case 'getAllResidences':
+      echo getAllResidences($conn, $currentUser);
+
+  }
+
+  //function to validate user and return currectUserType
+  function validateUser($currentUser, $currentUserType){
+    return $currentUserType=="applicant";
+  }
+
+  function getAllResidences($conn, $currentUser){
+    $message= array();
+
+    //get data from database
+    $sql = "SELECT * FROM residence;";
+    $results = $conn->query($sql);
+  //  $resultCheck = mysqli_num_rows($result); /*the number of row of the result*/
+    if ( $results->num_rows > 0) {
+
+    //loop the get each
+      while ( $row = $results->fetch_assoc()){
+
+        array_push($message, $row);
+      }
+    }
+    return json_encode($message);
+  }
+
+  //get search criteria data from user input and set variable
+  //$districtArea = $_POST['inputArea'];
+  //$userSearch = $_POST['inputSearch'];
+
+
+
+
+
+
+  //$result = mysqli_query($conn, $sql); /*database connection , database sql query code*/
+
+
+
+  //run this statement if the num of row is bigger than 0
+
+
+
+
+
+
+
+
+  //if failed to connect to database
+  if ( $conn->connect_error)
+  {
+	   die (" Connection failure" );
+  }
+
+
+
+
+
+
+
+
+
+/*
+  if($districtArea=='Damansara'){
+    $sql = "SELECT * FROM residence WHERE address LIKE "%50490%";" ;
+    $results = $conn->query($sql);
+
+
+    echo "<script type="text/javascript">
+
+
+    </script>";
+  }
+
+  elseif($districtArea=='Seputeh'){
+
+  }
+
+  elseif($districtArea=='Segambut'){
+
+  }
+
+  elseif($districtArea=='Kepong'){
+
+  }
+
+  elseif($districtArea=='KLCC'){
+
+  }
+
+  elseif($districtArea=='Setiawangsa'){
+
+  }
+
+  elseif($districtArea=='Bandar Tun Razak'){
+
+  }
+
+  elseif($districtArea=='Sungai Besi'){
+
+  }
+  else{
+    echo "SEARCH ERROR";
+  }
+
+*/
+
+
+/*
+  if ($resultCheck >0) {
+    //fetch each row of data into an array
+    while($row = mysqli_fetch_assoc($result)){
+      $residenceID = $row['residenceID'];
+      $residenceName = $row['residenceName'];
+
+      echo "<br>".$row['residenceID']." : ".$row['residenceName']."<br>";
+    }
+  }
+  */
+?>
